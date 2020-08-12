c133f556269339079b5a60fd2e76699b2cd8616b
luksri <luksri@gmail.com>
2020-08-12 08:26:10 +0530

    data frame output is formatted according to problem statement.

--- canopy_extract_table.py
+++ canopy_extract_table.py
@@ -4,14 +4,7 @@ import sys
 import xml.etree.ElementTree as ET
 import pandas as pd
 import numpy as np
-from collections import defaultdict
-
-
-# def df_empty(column, dtypes, index=None):
-#     df = pd.DataFrame(index=index)
-#     for c, d in zip(column, dtypes):
-#         df[c] = pd.Series(dtype=d)
-#     return df
+from dateutil.parser import parse
 
 
 def xml_first():
@@ -34,7 +27,7 @@ def xml_to_dataframe(xml_root, table_cell_props, total_no_of_rows):
     df = pd.DataFrame('', index=range(total_no_of_rows),
                       columns=['Booking Date', 'Txn Date', 'Booking Text', 'Value Date',
                                'Debit', 'Credit', 'Balance'])
-    # print(df.ndim, df.size, df.shape)
+
     row_num = -1
     for item in xml_root.findall('./page'):
         prev_cell_prop = sys.maxsize
@@ -47,12 +40,11 @@ def xml_to_dataframe(xml_root, table_cell_props, total_no_of_rows):
                     if int(child.attrib['left']) in range(int(cell_prop) - 55, int(cell_prop) + 55):
                         # print(df.loc[row_num, table_cell_props[cell_prop]], child.text)
                         if df.loc[row_num, table_cell_props[cell_prop]]:
-                            df.loc[row_num, table_cell_props[cell_prop]] += '\r\n'+child.text
+                            df.loc[row_num, table_cell_props[cell_prop]] += '\r\n' + child.text
                         else:
                             df.loc[row_num, table_cell_props[cell_prop]] = child.text
-
                     prev_cell_prop = child.attrib['left']
-    df.to_excel('output.xlsx', index=False)
+    return df
 
 
 def parse_xml_file():
@@ -80,11 +72,47 @@ def parse_xml_file():
                         table_headings_references[child.attrib['left']] = child.text
                     elif child.attrib['font'] == '3':
                         no_of_rows += 1
-        xml_to_dataframe(root, table_headings_references, no_of_rows)
+        return xml_to_dataframe(root, table_headings_references, no_of_rows)
     else:
         print('You need to generate the xml file')
+        return
+
+
+def format_date(entry):
+    try:
+        obj = parse(entry)
+        return obj.strftime('%Y/%m/%d')
+    except:
+        # print('exception is ' + str(sys.exc_info()))
+        pass
+
+
+def format_int(entry):
+    try:
+        entry = entry.replace(',', '')
+        entry = int(float(str(entry)))
+        return entry
+    except:
+        # print('exception is ' + str(sys.exc_info()))
+        pass
+
+
+def format_output(df):
+    df['Booking Date'] = df['Booking Date'].map(format_date)
+    df['Txn Date'] = df['Txn Date'].map(format_date)
+    df['Value Date'] = df['Value Date'].map(format_date)
+    df['Debit'] = df['Debit'].map(format_int)
+    df['Credit'] = df['Credit'].map(format_int)
+    df['Balance'] = df['Balance'].map(format_int)
+    df.to_excel('output.xlsx', index=False)
 
 
 if __name__ == '__main__':
     xml_first()
-    parse_xml_file()
+    df = parse_xml_file()
+    df.replace('', np.nan, inplace=True)
+    df.dropna(axis=0, how='all', inplace=True)
+    df.replace(np.nan, '', inplace=True)
+    if not df.empty:
+        format_output(df)
+    print("you can check the output now")
