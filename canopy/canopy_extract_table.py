7d9c7f83017fc447d4061539105bad5f4c78441c
luksri <luksri@gmail.com>
2020-08-12 12:36:55 +0530

    removed comments unnecessary

--- canopy/canopy_extract_table.py
+++ canopy/canopy_extract_table.py
@@ -40,9 +40,6 @@ def pdf_table_recreate(xml_root, table_cell_props, total_no_of_rows):
     :param total_no_of_rows: no of rows table data in xml file.
     :return: returns Pandas Dataframe contains entire table data.
     """
-    # table data is with font size 3.
-    # each cell data is determined with 'LEFT' attribute reference.
-
     df = pd.DataFrame('', index=range(total_no_of_rows),
                       columns=['Booking Date', 'Txn Date', 'Booking Text', 'Value Date',
                                'Debit', 'Credit', 'Balance'])
@@ -57,7 +54,6 @@ def pdf_table_recreate(xml_root, table_cell_props, total_no_of_rows):
                 for cell_prop in table_cell_props:
                     # check for the range to see which column it fits
                     if int(child.attrib['left']) in range(int(cell_prop) - 55, int(cell_prop) + 55):
-                        # print(df.loc[row_num, table_cell_props[cell_prop]], child.text)
                         if df.loc[row_num, table_cell_props[cell_prop]]:
                             df.loc[row_num, table_cell_props[cell_prop]] += '\r\n' + child.text
                         else:
@@ -81,11 +77,6 @@ def parse_xml_file():
         tree = ET.parse('canopy_technical_test_output.xml')
         # get root element
         root = tree.getroot()
-
-        # table uses different format styles. Here in the example, font size 2 and 3 has been used for table.
-        # font '2' : table heading
-        # font '3': table data
-        # the entire logic is based on referencing 'LEFT' attribute of table cell properties.
 
         no_of_rows = 0
         for item in root.findall('./page'):
